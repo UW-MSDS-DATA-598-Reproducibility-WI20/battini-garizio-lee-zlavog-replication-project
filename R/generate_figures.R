b2b3dbb23f29934eac0e6a0d58f04f5cf50f62da
Iacopo Garizio <igarizio@uc.cl>
2020-03-15 01:30:03 -0700

    tests for figures 1 and 2

---
+++ R/generate_figures.R
@@ -0,0 +1,8 @@
+generate_fig_2 <- function(){
+  source("analysis/F2_003_MakeFigure2aspdf.R")
+}
+
+generate_fig_3 <- function(){
+  source("analysis/F3_003_ComputeMainProjections.R")
+  source("analysis/F3_005_MakeFigure3aspdf.R")
+}
